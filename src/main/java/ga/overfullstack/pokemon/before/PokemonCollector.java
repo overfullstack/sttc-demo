d380499ddad5277d511988fe0b7bae349eb0de8d
Gopal S Akshintala <gopalakshintala@gmail.com>
2022-07-07 19:04:02 +0530

    Minor changes

--- src/main/java/ga/overfullstack/pokemon/before/PokemonCollector.java
+++ src/main/java/ga/overfullstack/pokemon/before/PokemonCollector.java
@@ -20,33 +20,36 @@ public class PokemonCollector {
     play(POKEMON_OFFSET_TO_FETCH, POKEMON_LIMIT_TO_FETCH);
   }
 
+  /**
+   * POOP (Procedure Oriented Programming)
+   */
   public static Map<String, String> play(int pokemonOffsetToFetch, int pokemonLimitToFetch) {
     validate(pokemonOffsetToFetch, pokemonLimitToFetch);
-    
+
     // Fetch all Pokémon
-    final var pokemonNames = HttpUtil.fetchAllPokemon(pokemonOffsetToFetch, pokemonLimitToFetch);
-    logger.info("Pokémon fetched: {}", pokemonNames);
+    final var fetchedPokemonNames = HttpUtil.fetchAllPokemon(pokemonOffsetToFetch, pokemonLimitToFetch);
+    logger.info("Pokémon fetched: {}", fetchedPokemonNames);
 
     // Find DB match for fetched Pokémon.
-    final var existingPokemonNameToPower = DBUtil.queryPokemonPowers(pokemonNames);
+    final var existingPokemonNameToPower = DBUtil.queryPokemonPowers(fetchedPokemonNames);
     logger.info("{} Matching Pokémon with Powers in DB: {}", existingPokemonNameToPower.size(), existingPokemonNameToPower);
 
-    // Fetch powers for missing Pokémon.
-    final var missingPokemonNames = pokemonNames.stream()
+    // Fetch powers for missing Pokémon (fetchedPokemonNames - existingPokemonNames).
+    final var missingPokemonNames = fetchedPokemonNames.stream()
         .filter(key -> !existingPokemonNameToPower.containsKey(key)).toList();
     logger.info("Fetch for {} missing Pokémon: {}", missingPokemonNames.size(), missingPokemonNames);
     final var newPokemonToInsert = missingPokemonNames.stream()
         .map(pokemonName -> new Pair<>(pokemonName, HttpUtil.fetchPokemonPower(pokemonName))).toList();
-    
+
     // Insert new fetched Pokémon into the DB.
     DBUtil.batchInsertPokemonPowers(newPokemonToInsert);
-    
+
     // Fetch all collected Pokémon in DB.
     final var allPokemonWithPowers = DBUtil.queryAllPokemonPowers();
     logger.info("{} Pokémon with Powers in DB: {}", allPokemonWithPowers.size(), allPokemonWithPowers);
     return allPokemonWithPowers;
   }
-  
+
   static void validate(int pokemonOffsetToFetch, int pokemonLimitToFetch) {
     if (pokemonOffsetToFetch < 0 || pokemonLimitToFetch < 0 || pokemonLimitToFetch > 10) {
       throw new IllegalArgumentException("Invalid offset or limit : offset=" + pokemonOffsetToFetch + ", limit=" + pokemonLimitToFetch);
