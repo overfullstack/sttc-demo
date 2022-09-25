9a83aa257810dfa19677d65ca0de27407ef7c124
Gopal S Akshintala <gopalakshintala@gmail.com>
2022-09-25 17:08:26 +0530

    Update blog post link
    Signed-off-by: Gopal S Akshintala <gopalakshintala@gmail.com>

--- src/main/java/ga/overfullstack/powermock/EntityLoader.java
+++ src/main/java/ga/overfullstack/powermock/EntityLoader.java
@@ -2,7 +2,13 @@ package ga.overfullstack.powermock;
 
 import java.lang.reflect.InvocationTargetException;
 
+/**
+ * Dummy class representing loading entity from DB
+ */
 public class EntityLoader {
+  private EntityLoader() {
+  }
+
   public static <T extends Entity> T loadNew(Class<T> entityType) {
     try {
       return entityType.getConstructor().newInstance();
